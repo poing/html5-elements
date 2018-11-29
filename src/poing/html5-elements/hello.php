cd864c5f3961824176739c9e9f8717fc262864c5
poing <brian.lavallee@invite-comm.jp>
2018-11-29 22:56:15 +0900

    basic

---
+++ src/poing/html5-elements/hello.php
@@ -0,0 +1,14 @@
+<?php
+namespace poing\html5-elements;
+/**
+ * A terribly useful class
+ *
+ * See: http://knpuniversity.com/screencast/question-answer-day
+ */
+class Hello
+{
+    public function sayHello()
+    {
+        return 'H3llo!';
+    }
+}
